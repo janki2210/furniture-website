1af48ca8c3b6d3cf063bc673a9729fb35d6ea33f
janki2210 <80520217+janki2210@users.noreply.github.com>
2022-02-25 16:32:31 +0530

    main

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
+
 let closer = document.querySelector('#closer');
 
 closer.onclick = () =>{
     closer.style.display = 'none';
     navbar.classList.remove('active');
     cart.classList.remove('active');
+    loginform.classList.remove('active');
 }
+
 
 let navbar = document.querySelector('.navbar');
 
@@ -18,4 +21,21 @@ let cart = document.querySelector('.shopping-cart');
 document.querySelector('#cart-btn').onclick = () =>{
     closer.style.display = 'block';
     cart.classList.toggle('active');
+}
+
+let loginform = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+    closer.style.display = 'block';
+    loginform.classList.toggle('active');
+}
+
+
+let searchform = document.querySelector('.header .search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchform.classList.toggle('active');
+}
+window.onscroll = () =>{
+    searchform.classList.remove('active');
 }
